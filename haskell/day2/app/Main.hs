55508bb000dfbcb90431121b9d6581492fe54be8
Esgariot <esgariot@icloud.com>
2022-12-04 22:34:50 +0100

    haskell: Day2 part2.

--- haskell/day2/app/Main.hs
+++ haskell/day2/app/Main.hs
@@ -1,27 +1,40 @@
+{-# LANGUAGE DataKinds #-}
+
 module Main where
 
 import Control.Monad (void)
 import Data.Either (rights)
 import System.Environment (getArgs)
 import System.Exit (exitFailure)
-import Text.ParserCombinators.Parsec (Parser, oneOf, parse, space, (<|>))
+import Text.ParserCombinators.Parsec (Parser, char, parse, space, (<|>))
 
-data Hand = Rock | Paper | Scissors deriving (Eq)
+data Player = Me | Opponent
 
-data Outcome = Win | Loss | Draw deriving (Show)
+data Hand a = Rock | Paper | Scissors deriving (Eq)
 
-data Round = Round {player :: Hand, opponent :: Hand}
+data Outcome = Win | Loss | Draw deriving (Eq)
 
-vs :: Hand -> Hand -> Outcome
+data Round = Round {opponent :: Hand Opponent, outcome :: Outcome}
+
+vs :: Hand Me -> Hand Opponent -> Outcome
 Paper `vs` Rock = Win
 Rock `vs` Scissors = Win
 Scissors `vs` Paper = Win
 one `vs` other
-  | one == other = Draw
+  | un one == un other = Draw
   | otherwise = Loss
+  where
+    un Rock = Rock
+    un Paper = Paper
+    un Scissors = Scissors
 
-score :: Round -> Int
-score Round {player, opponent} = handScore player + roundScore (player `vs` opponent)
+strategy :: Round -> Hand Me
+strategy Round {opponent, outcome} = head $ (filter $ match outcome) [Rock, Paper, Scissors]
+  where
+    match expected hand = expected == hand `vs` opponent
+
+score :: Hand Me -> Hand Opponent -> Int
+score player opponent = handScore player + roundScore (player `vs` opponent)
   where
     handScore Rock = 1
     handScore Paper = 2
@@ -34,21 +47,27 @@ parseArgs :: [FilePath] -> IO String
 parseArgs [input] = readFile input
 parseArgs _ = exitFailure
 
-parseHand :: Parser Hand
+parseHand :: Parser (Hand a)
 parseHand =
-  Rock <$ oneOf "AX"
-    <|> Paper <$ oneOf "BY"
-    <|> Scissors <$ oneOf "CZ"
+  Rock <$ char 'A'
+    <|> Paper <$ char 'B'
+    <|> Scissors <$ char 'C'
+
+parseOutcome :: Parser Outcome
+parseOutcome =
+  Loss <$ char 'X'
+    <|> Draw <$ char 'Y'
+    <|> Win <$ char 'Z'
 
 parseRound :: Parser Round
 parseRound = do
   opponent <- parseHand
   void $ space
-  player <- parseHand
-  return Round {player, opponent}
+  outcome <- parseOutcome
+  return Round {opponent, outcome}
 
 parseInput :: String -> [Round]
 parseInput input = rights $ parse parseRound "" <$> (lines input)
 
 main :: IO ()
-main = getArgs >>= parseArgs >>= print . sum . map score . parseInput
+main = getArgs >>= parseArgs >>= print . sum . map (\r@Round {opponent} -> score (strategy r) opponent) . parseInput
